25d359c3278d3be545075a59770bfab2563b4e38
Denzell Ford <fdenzell@gmail.com>
2023-05-21 14:12:37 -0500

    ui: Finished the basic view for the frontend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 import node from "@astrojs/node";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), svelte()],
   output: 'server',
   adapter: node({
     mode: "standalone"
