5552eec698ab5f9547022365e85cee62596d99ae
Nicholas Khami <nicholas.khami@gmail.com>
2023-05-27 15:08:09 -0500

    skeleton: user details page

--- src/components/RegisterOrUserProfile.tsx
+++ src/components/RegisterOrUserProfile.tsx
@@ -1,9 +1,6 @@
 import { Menu, MenuItem, Popover, PopoverButton, PopoverPanel, Transition } from 'solid-headless'
-import {
-	BiRegularLogIn,
-	BiRegularLogOut,
-	BiRegularUser,
-} from 'solid-icons/bi'
+import { BiRegularLogIn, BiRegularLogOut, BiRegularUser } from 'solid-icons/bi'
+import { AiOutlineProfile } from 'solid-icons/ai'
 import { IoSettingsOutline } from 'solid-icons/io'
 import { createEffect, createSignal } from 'solid-js'
 
@@ -100,14 +97,14 @@ const RegisterOrUserProfile = () => {
 										class="absolute left-1/2 z-10 mt-5 -translate-x-[90%] transform px-4 sm:px-0"
 									>
 										<Menu class="flex flex-col space-y-1 overflow-hidden rounded-lg border border-slate-900 bg-neutral-100 p-1 shadow-lg drop-shadow-lg dark:bg-neutral-700 dark:text-white">
-											<MenuItem as="button" aria-label="Empty" />
+											<MenuItem class="h-0" as="button" aria-label="Empty" />
 											<MenuItem
-												as="button"
+												as="a"
 												class="flex space-x-2 rounded-md px-2 py-1 hover:cursor-pointer focus:bg-neutral-100 focus:outline-none dark:hover:bg-neutral-600 dark:hover:bg-none dark:focus:bg-neutral-600"
-												onClick={logout}
+												href="/user/"
 											>
-												<BiRegularLogOut class="h-6 w-6" />
-												<div class="text-md font-medium">Logout</div>
+												<AiOutlineProfile class="h-6 w-6" />
+												<div class="text-md font-medium">Profile</div>
 											</MenuItem>
 											<MenuItem
 												as="a"
@@ -116,6 +113,14 @@ const RegisterOrUserProfile = () => {
 											>
 												<IoSettingsOutline class="h-6 w-6" />
 												<div class="text-md font-medium">Settings</div>
+											</MenuItem>
+											<MenuItem
+												as="button"
+												class="flex space-x-2 rounded-md px-2 py-1 hover:cursor-pointer focus:bg-neutral-100 focus:outline-none dark:hover:bg-neutral-600 dark:hover:bg-none dark:focus:bg-neutral-600"
+												onClick={logout}
+											>
+												<BiRegularLogOut class="h-6 w-6" />
+												<div class="text-md font-medium">Logout</div>
 											</MenuItem>
 										</Menu>
 									</PopoverPanel>
