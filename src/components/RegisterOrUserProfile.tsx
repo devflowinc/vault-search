7c18f0e538001d73e3e81c3da28859f9085929ff
Nicholas Khami <nicholas.khami@gmail.com>
2023-05-27 19:02:38 -0500

    feature: update and view user information

--- src/components/RegisterOrUserProfile.tsx
+++ src/components/RegisterOrUserProfile.tsx
@@ -3,12 +3,13 @@ import { BiRegularLogIn, BiRegularLogOut, BiRegularUser } from 'solid-icons/bi'
 import { AiOutlineProfile } from 'solid-icons/ai'
 import { IoSettingsOutline } from 'solid-icons/io'
 import { createEffect, createSignal } from 'solid-js'
+import { isUserDTO, type UserDTO } from '../../utils/apiTypes'
 
 const RegisterOrUserProfile = () => {
 	const apiHost = import.meta.env.PUBLIC_API_HOST
 
 	const [isLoadingUser, setIsLoadingUser] = createSignal(true)
-	const [signedInUser, setSignedInUser] = createSignal(false)
+	const [currentUser, setCurrentUser] = createSignal<UserDTO | null>(null)
 
 	const logout = () => {
 		void fetch(`${apiHost}/auth`, {
@@ -34,7 +35,13 @@ const RegisterOrUserProfile = () => {
 			credentials: 'include'
 		}).then((response) => {
 			if (response.ok) {
-				setSignedInUser(true)
+				response.json().then((data) => {
+					if (isUserDTO(data)) {
+						setCurrentUser(data)
+						setIsLoadingUser(false)
+					}
+				})
+				return
 			}
 			setIsLoadingUser(false)
 		})
@@ -52,7 +59,7 @@ const RegisterOrUserProfile = () => {
 				leaveTo="opacity-0 -translate-y-1 scale-50"
 			>
 				<Transition
-					show={!signedInUser()}
+					show={!currentUser()}
 					enter="transition duration-700"
 					enterFrom="opacity-0 -translate-y-1 scale-50"
 					enterTo="opacity-100 translate-y-0 scale-100"
@@ -69,7 +76,7 @@ const RegisterOrUserProfile = () => {
 					</a>
 				</Transition>
 				<Transition
-					show={signedInUser()}
+					show={!!currentUser()}
 					enter="transition duration-700"
 					enterFrom="opacity-0 -translate-y-1 scale-50"
 					enterTo="opacity-100 translate-y-0 scale-100"
@@ -101,7 +108,7 @@ const RegisterOrUserProfile = () => {
 											<MenuItem
 												as="a"
 												class="flex space-x-2 rounded-md px-2 py-1 hover:cursor-pointer focus:bg-neutral-100 focus:outline-none dark:hover:bg-neutral-600 dark:hover:bg-none dark:focus:bg-neutral-600"
-												href="/user/"
+												href={`/user/${currentUser()?.id}`}
 											>
 												<AiOutlineProfile class="h-6 w-6" />
 												<div class="text-md font-medium">Profile</div>
