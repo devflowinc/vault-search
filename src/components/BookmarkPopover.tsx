7de14fbb880d00b6b69cf38df4346cf846c17bb8
Denzell Ford <fdenzell@gmail.com>
2023-06-14 01:10:27 -0500

    wip: Working on new bookmark form

--- src/components/BookmarkPopover.tsx
+++ src/components/BookmarkPopover.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { For, Match, Switch, createSignal } from "solid-js";
 import { Popover, PopoverButton, PopoverPanel, Transition } from 'solid-headless'
 import usePopper from "solid-popper";
 import { RiSystemAddFill } from "solid-icons/ri";
@@ -12,6 +12,11 @@ export interface BookmarkPopoverProps {
 const BookmarkPopover = (props: BookmarkPopoverProps) => {
 	const [anchor, setAnchor] = createSignal<HTMLButtonElement>();
 	const [popper, setPopper] = createSignal<HTMLDivElement>();
+
+	const [showCollectionForm, setShowCollectionForm] = createSignal(false);
+
+	const [collectionFormTitle, setCollectionFormTitle] = createSignal("");
+	const [collectionFormDescription, setCollectionFormDescription] = createSignal("");
 
 	usePopper(anchor, popper, {
 		placement: 'auto',
@@ -29,7 +34,7 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 	});
 
 	return (
-		<Popover defaultOpen={false} class="relative">
+		<Popover defaultOpen={true} class="relative">
 			{({ isOpen }) => (
 				<div>
 					<PopoverButton ref={setAnchor}>
@@ -41,20 +46,39 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 					>
 						<PopoverPanel ref={setPopper} class="absolute w-screen max-w-sm">
 							<div class="flex flex-col bg-white w-full drop-shadow-md overflow-hidden">
-								<div class="w-full p-2">
+								<div class="w-full font-bold p-2 text-lg">
 									Add card to collection
 								</div>
-								{...props.cardCollections.map((collection) => (
-									<div>
-										<div class="w-full p-2">
-											{collection.name}
+								<For each={props.cardCollections}>
+									{(collection) => (
+										<div>
+											<div class="w-full p-2">
+												{collection.name}
+											</div>
 										</div>
+
+									)}
+								</For>
+								{showCollectionForm() && (
+									<div class="p-2">
+										<input
+											value={collectionFormTitle()}
+											onInput={(e) => setCollectionFormTitle(e.target.value)}
+											type="text"
+										/>
+										<input
+											value={collectionFormDescription()}
+											onInput={(e) => setCollectionFormDescription(e.target.value)}
+											type="text"
+										/>
 									</div>
-								))}
-								<button class="flex space-x-2 w-full bg-neutral-100 p-2">
-									<RiSystemAddFill class="h-5 w-5" />
-									<p> New Colletion </p>
-								</button>
+								)}
+								{!showCollectionForm() && (
+									<button onClick={() => { setShowCollectionForm(true) }} class="flex space-x-2 w-full bg-neutral-100 p-2">
+										<RiSystemAddFill class="h-5 w-5" />
+										<p> New Colletion </p>
+									</button>
+								)}
 							</div>
 						</PopoverPanel>
 					</Transition>
