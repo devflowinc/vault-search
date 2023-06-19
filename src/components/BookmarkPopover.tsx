81257e5060ff24cbd6e37e55cd0abdeca8d747ec
Denzell Ford <fdenzell@gmail.com>
2023-06-19 16:58:20 -0500

    feature: Refetch Collections on submit and dark mode

--- src/components/BookmarkPopover.tsx
+++ src/components/BookmarkPopover.tsx
@@ -7,7 +7,8 @@ import InputRowsForm from "./Atoms/InputRowsForm";
 
 export interface BookmarkPopoverProps {
 	card: ScoreCardDTO,
-	cardCollections: CardCollectionDTO[]
+	cardCollections: CardCollectionDTO[],
+	fetchCardCollections: () => void,
 }
 
 const BookmarkPopover = (props: BookmarkPopoverProps) => {
@@ -19,7 +20,6 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 	const [showCollectionForm, setShowCollectionForm] = createSignal(false);
 
 	const [collectionFormTitle, setCollectionFormTitle] = createSignal("");
-	const [collectionFormDescription, setCollectionFormDescription] = createSignal("");
 
 	usePopper(anchor, popper, {
 		placement: 'auto',
@@ -53,7 +53,7 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 						leaveTo="opacity-0 translate-y-2"
 					>
 						<PopoverPanel unmount={false} ref={setPopper} class="absolute w-screen max-w-sm">
-							<Menu class="flex flex-col bg-white w-full drop-shadow-md overflow-hidden">
+							<Menu class="flex flex-col bg-white w-full drop-shadow-md overflow-hidden dark:bg-shark-500">
 								<div class="w-full font-bold p-2 text-lg">
 									Add card to collection
 								</div>
@@ -61,8 +61,8 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 								<For each={props.cardCollections}>
 									{(collection) => {
 										return (
-											<MenuItem
-												class="flex justify-between p-2 hover:bg-gray-100"
+											<PopoverButton
+												class="flex justify-between p-2 hover:bg-gray-100 dark:hover:bg-neutral-500/80"
 												onClick={() => {
 													fetch(`${apiHost}/card_collection/${collection.id}`, {
 														method: 'POST',
@@ -75,66 +75,58 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 														})
 													}).then((response) => {
 														if (response.ok) {
-															response.json().then((data) => {
-																console.log(data)
-															})
-														} else {
-															response.json().then((data) => {
-																console.log(data)
-															})
 														}
 													});
 												}}
 												as="button">
 												<p>{collection.name} </p>
-												<div class="rounded-md text-white px-2 bg-magenta-500">
+												<div class="rounded-md text-white px-2 bg-magenta-500 dark:bg-turquoise-400 dark:text-black">
 													save
 												</div>
-											</MenuItem>
+											</PopoverButton>
 										);
 									}}
 								</For>
 								{showCollectionForm() && (
-									<InputRowsForm
-										createButtonText="Create collection"
-										onCreate={() => {
-											fetch(`${apiHost}/card_collection`, {
-												method: 'POST',
-												headers: {
-													'Content-Type': 'application/json'
+									<div class="bg-gray-100 dark:bg-neutral-500/80">
+										<div class="font-bold pt-2 px-2 text-lg">New Collection</div>
+										<InputRowsForm
+											createButtonText="Create collection"
+											onCreate={() => {
+												const title = collectionFormTitle();
+												if (title.trim() == "") return;
+
+												fetch(`${apiHost}/card_collection`, {
+													method: 'POST',
+													headers: {
+														'Content-Type': 'application/json'
+													},
+													credentials: 'include',
+													body: JSON.stringify({
+														name: title,
+														description: "",
+														is_public: true,
+													})
+												}).then(() => {
+													props.fetchCardCollections()
+													setShowCollectionForm(false)
+												});
+											}}
+											onCancel={() => {
+												setShowCollectionForm(false);
+											}}
+											inputGroups={[
+												{
+													label: "Title",
+													inputValue: collectionFormTitle,
+													setInputValue: setCollectionFormTitle
 												},
-												credentials: 'include',
-												body: JSON.stringify({
-													name: collectionFormTitle(),
-													description: collectionFormDescription(),
-													is_public: true,
-												})
-											}).then((response) => {
-												if (response.ok) {
-													response.json().then((data) => {
-														console.log(data)
-													})
-												} else {
-													response.json().then((data) => {
-														console.log(data)
-													})
-												}
-											});
-										}}
-										onCancel={() => {
-											setShowCollectionForm(false);
-										}}
-										inputGroups={[
-											{
-												label: "Title",
-												inputValue: collectionFormTitle,
-												setInputValue: setCollectionFormTitle
-											},
-										]}
-									/>
+											]}
+										/>
+									</div>
 								)}
 								{!showCollectionForm() && (
-									<MenuItem as="button" onClick={() => { setShowCollectionForm(true) }} class="flex space-x-2 w-full bg-neutral-100 p-2">
+									<MenuItem as="button" onClick={() => { setShowCollectionForm(true) }} class="flex space-x-2 w-full bg-neutral-100 p-2 dark:bg-neutral-500/80">
 										<RiSystemAddFill class="h-5 w-5" />
 										<p> New Colletion </p>
 									</MenuItem>
