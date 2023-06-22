c5bd19f298736db0de43a471fd99d70954bafd7f
densumesh <dens.sumesh79@gmail.com>
2023-06-22 15:51:39 -0700

    bugfix: popover closes randomly

--- src/components/BookmarkPopover.tsx
+++ src/components/BookmarkPopover.tsx
@@ -1,25 +1,25 @@
-import { For, Match, Switch, createSignal } from "solid-js";
+import { For, Match, Switch, createSignal } from 'solid-js'
 import { Menu, MenuItem, Popover, PopoverButton, PopoverPanel, Transition } from 'solid-headless'
-import usePopper from "solid-popper";
-import { RiSystemAddFill, RiSystemCheckFill } from "solid-icons/ri";
-import type { CardCollectionDTO, ScoreCardDTO } from "../../utils/apiTypes";
-import InputRowsForm from "./Atoms/InputRowsForm";
+import usePopper from 'solid-popper'
+import { RiSystemAddFill, RiSystemCheckFill } from 'solid-icons/ri'
+import type { CardCollectionDTO, ScoreCardDTO } from '../../utils/apiTypes'
+import InputRowsForm from './Atoms/InputRowsForm'
 
 export interface BookmarkPopoverProps {
-	card: ScoreCardDTO,
-	cardCollections: CardCollectionDTO[],
-	fetchCardCollections: () => void,
+	card: ScoreCardDTO
+	cardCollections: CardCollectionDTO[]
+	fetchCardCollections: () => void
 }
 
 const BookmarkPopover = (props: BookmarkPopoverProps) => {
 	const apiHost = import.meta.env.PUBLIC_API_HOST
 
-	const [anchor, setAnchor] = createSignal<HTMLButtonElement>();
-	const [popper, setPopper] = createSignal<HTMLDivElement>();
+	const [anchor, setAnchor] = createSignal<HTMLButtonElement>()
+	const [popper, setPopper] = createSignal<HTMLDivElement>()
 
-	const [showCollectionForm, setShowCollectionForm] = createSignal(false);
+	const [showCollectionForm, setShowCollectionForm] = createSignal(false)
 
-	const [collectionFormTitle, setCollectionFormTitle] = createSignal("");
+	const [collectionFormTitle, setCollectionFormTitle] = createSignal('')
 
 	usePopper(anchor, popper, {
 		placement: 'auto',
@@ -27,18 +27,18 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 			{
 				name: 'offset',
 				options: {
-					offset: [10, 20],
-				},
+					offset: [10, 20]
+				}
 			},
 			{
-				name: 'preventOverflow',
-			},
-		],
-	});
+				name: 'preventOverflow'
+			}
+		]
+	})
 
 	return (
 		<Popover defaultOpen={false} class="relative">
-			{({ isOpen }) => (
+			{({ isOpen, setState }) => (
 				<div>
 					<PopoverButton ref={setAnchor}>
 						<RiSystemAddFill class="h-5 w-5" />
@@ -53,10 +53,8 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 						leaveTo="opacity-0 translate-y-2"
 					>
 						<PopoverPanel unmount={false} ref={setPopper} class="absolute w-screen max-w-sm">
-							<Menu class="flex flex-col bg-white w-full drop-shadow-md overflow-hidden dark:bg-shark-500">
-								<div class="w-full font-bold p-2 text-lg">
-									Add card to collection
-								</div>
+							<Menu class="flex w-full flex-col overflow-hidden bg-white drop-shadow-md dark:bg-shark-500">
+								<div class="w-full p-2 text-lg font-bold">Add card to collection</div>
 								<MenuItem as="button" aria-label="Empty" />
 								<For each={props.cardCollections}>
 									{(collection) => {
@@ -71,30 +69,32 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 														},
 														credentials: 'include',
 														body: JSON.stringify({
-															card_metadata_id: props.card.metadata.id,
+															card_metadata_id: props.card.metadata.id
 														})
 													}).then((response) => {
 														if (response.ok) {
 														}
-													});
+													})
+													setState(true)
 												}}
-												as="button">
+												as="button"
+											>
 												<p>{collection.name} </p>
-												<div class="rounded-md text-white px-2 bg-magenta-500 dark:bg-turquoise-400 dark:text-black">
+												<div class="rounded-md bg-magenta-500 px-2 text-white dark:bg-turquoise-400 dark:text-black">
 													save
 												</div>
 											</PopoverButton>
-										);
+										)
 									}}
 								</For>
 								{showCollectionForm() && (
 									<div class="bg-gray-100 dark:bg-neutral-500/80">
-										<div class="font-bold pt-2 px-2 text-lg">New Collection</div>
+										<div class="px-2 pt-2 text-lg font-bold">New Collection</div>
 										<InputRowsForm
 											createButtonText="Create collection"
 											onCreate={() => {
-												const title = collectionFormTitle();
-												if (title.trim() == "") return;
+												const title = collectionFormTitle()
+												if (title.trim() == '') return
 
 												fetch(`${apiHost}/card_collection`, {
 													method: 'POST',
@@ -104,31 +104,41 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 													credentials: 'include',
 													body: JSON.stringify({
 														name: title,
-														description: "",
-														is_public: true,
+														description: '',
+														is_public: true
 													})
 												}).then(() => {
 													props.fetchCardCollections()
 													setShowCollectionForm(false)
-												});
+													setCollectionFormTitle('')
+													setState(true)
+												})
 											}}
 											onCancel={() => {
-												setShowCollectionForm(false);
+												setShowCollectionForm(false)
+												setState(true)
 											}}
 											inputGroups={[
 												{
-													label: "Title",
+													label: 'Title',
 													inputValue: collectionFormTitle,
 													setInputValue: setCollectionFormTitle
-												},
+												}
 											]}
 										/>
 									</div>
 								)}
 								{!showCollectionForm() && (
-									<MenuItem as="button" onClick={() => { setShowCollectionForm(true) }} class="flex space-x-2 w-full bg-neutral-100 p-2 dark:bg-neutral-500/80">
+									<MenuItem
+										as="button"
+										onClick={() => {
+											setShowCollectionForm(true)
+											setState(true)
+										}}
+										class="flex w-full space-x-2 bg-neutral-100 p-2 dark:bg-neutral-500/80"
+									>
 										<RiSystemAddFill class="h-5 w-5" />
-										<p> New Colletion </p>
+										<p> New Collection </p>
 									</MenuItem>
 								)}
 							</Menu>
@@ -137,7 +147,7 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
 				</div>
 			)}
 		</Popover>
-	);
-};
+	)
+}
 
-export default BookmarkPopover;
+export default BookmarkPopover
