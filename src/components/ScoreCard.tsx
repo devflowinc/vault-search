b93a13e9b1366c365cbcc8c53566d8f481b16fba
densumesh <dens.sumesh79@gmail.com>
2023-06-28 15:57:48 -0700

    Revert "feature: copy file link to clipboard"
    This reverts commit 1ebadf751f3f51ac1191b3172d6b3816b7574b13.

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -14,7 +14,6 @@ import {
   RiSystemArrowUpCircleLine,
 } from "solid-icons/ri";
 import BookmarkPopover from "./BookmarkPopover";
-import { VsFileSymlinkFile } from "solid-icons/vs";
 
 export interface ScoreCardProps {
   cardCollections: CardCollectionDTO[];
@@ -183,15 +182,7 @@ const ScoreCard = (props: ScoreCardProps) => {
             </p>
           </div>
         </div>
-        <div class="flex gap-x-1">
-          <VsFileSymlinkFile
-            class="h-5 w-5 cursor-pointer"
-            onClick={() => {
-              navigator.clipboard.writeText(
-                "https://vault.arguflow.com/card/" + props.card.metadata.id,
-              );
-            }}
-          />
+        <div>
           <BookmarkPopover
             cardCollections={props.cardCollections}
             card={props.card}
