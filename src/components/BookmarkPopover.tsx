5f1e31fbbe7b245100a567417e404c587a6c4422
Nicholas Khami <nicholas.khami@gmail.com>
2023-07-01 00:46:49 -0500

    bugfix + styling: proper z-index'ing on BookmarkPopover and styling to be more consistent

--- src/components/BookmarkPopover.tsx
+++ src/components/BookmarkPopover.tsx
@@ -1,11 +1,10 @@
-import { For, Setter, createEffect, createSignal } from "solid-js";
+import { For, Setter, Show, createEffect, createSignal } from "solid-js";
 import {
   Menu,
   MenuItem,
   Popover,
   PopoverButton,
   PopoverPanel,
-  Transition,
 } from "solid-headless";
 import { RiSystemAddFill } from "solid-icons/ri";
 import type {
@@ -77,77 +76,72 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
               fetchCollections();
             }}
           >
-            <RiSystemAddFill class="h-5 w-5 fill-current" />
+            <RiSystemAddFill class="z-0 h-5 w-5 fill-current" />
           </PopoverButton>
-          <Transition
-            show={isOpen() || usingPanel()}
-            enter="transition duration-200"
-            enterFrom="opacity-0 translate-y-2"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition duration-200"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-2"
-          >
+          <Show when={isOpen() || usingPanel()}>
             <PopoverPanel
               unmount={false}
-              class="absolute w-screen max-w-xs -translate-x-[300px]"
+              class="absolute z-50 w-screen max-w-xs -translate-x-[300px]"
               onMouseEnter={() => setUsingPanel(true)}
               onMouseLeave={() => setUsingPanel(false)}
               onClick={() => setState(true)}
             >
-              <Menu class=" flex w-full flex-col justify-end overflow-hidden bg-white drop-shadow-md dark:bg-shark-500">
-                <div class="w-full p-2 text-lg font-bold">
-                  Add card to collection
+              <Menu class=" flex w-full flex-col justify-end space-y-2 overflow-hidden rounded bg-white py-4 drop-shadow-md dark:bg-shark-700">
+                <div class="mb-3 w-full px-4 text-center text-lg font-bold">
+                  Manage Collections For This Card
                 </div>
                 <MenuItem as="button" aria-label="Empty" />
-                <div class="scrollbar-track-rounded-md scrollbar-thumb-rounded-md max-w-screen max-h-[20vh] transform justify-end overflow-y-auto rounded scrollbar-thin scrollbar-track-neutral-200 scrollbar-thumb-neutral-400 dark:scrollbar-track-neutral-700 dark:scrollbar-thumb-neutral-600">
+                <div class="scrollbar-track-rounded-md scrollbar-thumb-rounded-md max-w-screen max-h-[20vh] transform justify-end space-y-2 overflow-y-auto rounded px-4 scrollbar-thin scrollbar-track-neutral-200 scrollbar-thumb-neutral-400 dark:scrollbar-track-neutral-700 dark:scrollbar-thumb-neutral-600">
                   <For each={props.cardCollections}>
-                    {(collection) => {
+                    {(collection, idx) => {
                       return (
-                        <div class="flex w-full items-center justify-between p-2 hover:bg-gray-100 dark:hover:bg-neutral-500/80">
-                          <p class="flex flex-row justify-start">
-                            {collection.name}{" "}
-                          </p>
+                        <>
+                          <Show when={idx() != 0}>
+                            <div class="h-px w-full bg-neutral-200 dark:bg-neutral-700" />
+                          </Show>
+                          <div class="flex w-full items-center justify-between space-x-2">
+                            <p class="max-w-[80%]">{collection.name}</p>
 
-                          <input
-                            type="checkbox"
-                            checked={cardCollections()?.some(
-                              (c) => c.collection_id == collection.id,
-                            )}
-                            onChange={(e) => {
-                              void fetch(
-                                `${apiHost}/card_collection/${collection.id}`,
-                                {
-                                  method: e.currentTarget.checked
-                                    ? "POST"
-                                    : "DELETE",
-                                  headers: {
-                                    "Content-Type": "application/json",
+                            <input
+                              type="checkbox"
+                              checked={cardCollections()?.some(
+                                (c) => c.collection_id == collection.id,
+                              )}
+                              onChange={(e) => {
+                                void fetch(
+                                  `${apiHost}/card_collection/${collection.id}`,
+                                  {
+                                    method: e.currentTarget.checked
+                                      ? "POST"
+                                      : "DELETE",
+                                    headers: {
+                                      "Content-Type": "application/json",
+                                    },
+                                    credentials: "include",
+                                    body: JSON.stringify({
+                                      card_metadata_id: props.card.metadata.id,
+                                    }),
                                   },
-                                  credentials: "include",
-                                  body: JSON.stringify({
-                                    card_metadata_id: props.card.metadata.id,
-                                  }),
-                                },
-                              ).then((response) => {
-                                if (!response.ok) {
-                                  e.currentTarget.checked =
-                                    !e.currentTarget.checked;
-                                }
-                              });
-                              setState(true);
-                            }}
-                            class="h-4 w-4 rounded-sm	border-gray-300 bg-neutral-500 accent-turquoise focus:ring-neutral-200 dark:border-neutral-700 dark:focus:ring-neutral-600"
-                          />
-                        </div>
+                                ).then((response) => {
+                                  if (!response.ok) {
+                                    e.currentTarget.checked =
+                                      !e.currentTarget.checked;
+                                  }
+                                });
+                                setState(true);
+                              }}
+                              class="h-4 w-4 rounded-sm	border-gray-300 bg-neutral-500 accent-turquoise focus:ring-neutral-200 dark:border-neutral-700 dark:focus:ring-neutral-600"
+                            />
+                          </div>
+                        </>
                       );
                     }}
                   </For>
                 </div>
                 {showCollectionForm() && (
-                  <div class="bg-gray-100 dark:bg-neutral-500/80">
-                    <div class="px-2 pt-2 text-lg font-bold">
-                      New Collection
+                  <div class="mx-4 rounded bg-gray-100 py-2 dark:bg-neutral-800">
+                    <div class="px-2 text-lg font-bold">
+                      Create New Collection
                     </div>
                     <InputRowsForm
                       createButtonText="Create collection"
@@ -187,21 +181,23 @@ const BookmarkPopover = (props: BookmarkPopoverProps) => {
                   </div>
                 )}
                 {!showCollectionForm() && (
-                  <MenuItem
-                    as="button"
-                    onClick={() => {
-                      setShowCollectionForm(true);
-                      setState(true);
-                    }}
-                    class="flex w-full space-x-2 bg-neutral-100 p-2 dark:bg-neutral-500/80"
-                  >
-                    <RiSystemAddFill class="h-5 w-5  fill-current" />
-                    <p> New Collection </p>
-                  </MenuItem>
+                  <div class="px-4 pt-4">
+                    <MenuItem
+                      as="button"
+                      onClick={() => {
+                        setShowCollectionForm(true);
+                        setState(true);
+                      }}
+                      class="flex w-full items-center justify-center rounded-full border border-green-500 bg-transparent px-2 text-lg text-green-500"
+                    >
+                      <RiSystemAddFill class="h-5 w-5 fill-current" />
+                      <p> Create New Collection </p>
+                    </MenuItem>
+                  </div>
                 )}
               </Menu>
             </PopoverPanel>
-          </Transition>
+          </Show>
         </div>
       )}
     </Popover>
