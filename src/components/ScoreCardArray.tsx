f166b89bfce777846b524840f2cc37bcde862eb8
Nicholas Khami <nicholas.khami@gmail.com>
2023-07-04 18:45:58 -0500

    styling: reduce layout shift on navigation

--- src/components/ScoreCardArray.tsx
+++ src/components/ScoreCardArray.tsx
@@ -1,7 +1,7 @@
 import { Show, createSignal } from "solid-js";
 import type { CardMetadataWithVotes } from "../../utils/apiTypes";
 import type { ScoreCardProps } from "./ScoreCard";
-import { FiChevronLeft } from "solid-icons/fi";
+import { FiChevronLeft, FiChevronRight } from "solid-icons/fi";
 import ScoreCard from "./ScoreCard";
 
 export type ScoreCardAraryProps = Omit<ScoreCardProps, "card"> & {
@@ -12,18 +12,28 @@ export const ScoreCardArray = (props: ScoreCardAraryProps) => {
   const [curCard, setCurCard] = createSignal(0);
 
   return (
-    <div class="flex space-x-2">
-      <Show when={curCard() > 0}>
-        <button onClick={() => setCurCard((prev) => prev - 1)}>
-          <FiChevronLeft class="h-8 w-8 fill-current" />
-        </button>
-      </Show>
+    <div class="mx-auto flex items-center">
+      <div class="w-[32px]">
+        <Show when={curCard() > 0}>
+          <button onClick={() => setCurCard((prev) => prev - 1)}>
+            <FiChevronLeft class="h-8 w-8" />
+          </button>
+        </Show>
+        <Show when={curCard() <= 0}>
+          <FiChevronLeft class="h-8 w-8 text-transparent" />
+        </Show>
+      </div>
       <ScoreCard {...props} card={props.cards[curCard()]} />
-      <Show when={curCard() < props.cards.length - 1}>
-        <button onClick={() => setCurCard((prev) => prev + 1)}>
-          <FiChevronLeft class="h-8 w-8 rotate-180 transform fill-current" />
-        </button>
-      </Show>
+      <div class="w-[32px]">
+        <Show when={curCard() < props.cards.length - 1}>
+          <button onClick={() => setCurCard((prev) => prev + 1)}>
+            <FiChevronRight class="h-8 w-8" />
+          </button>
+        </Show>
+        <Show when={curCard() >= props.cards.length - 1}>
+          <FiChevronRight class="h-8 w-8 text-transparent" />
+        </Show>
+      </div>
     </div>
   );
 };
